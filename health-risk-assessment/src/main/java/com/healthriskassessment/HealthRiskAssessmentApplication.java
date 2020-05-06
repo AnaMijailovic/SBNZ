3aaa81808bbea11eb4e9a10f4227d19701d4cbea
AnaMijailovic <anaam1997@gmail.com>
2020-05-06 22:57:02 +0200

    initial spring project

---
+++ health-risk-assessment/src/main/java/com/healthriskassessment/HealthRiskAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.healthriskassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthRiskAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthRiskAssessmentApplication.class, args);
+	}
+
+}
